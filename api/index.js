fd3df92b06dcaa6b7a2c443d5c93fe6d68414806
ercooss <95519428+ercooss@users.noreply.github.com>
2021-12-06 11:53:52 +0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -2,8 +2,8 @@ var express = require('express');
 var r = express.Router();
 
 // load pre-trained model
-const model = require('./sdk/model1.js'); //predict
-const cls_model = require('./sdk/cls_model1.js'); //cls
+const model = require('api/sdk/model1.js'); //predict 
+const cls_model = require('api/sdk/cls_model1.Js'); //cls
 
 // Bot Setting
 const TelegramBot = require('node-telegram-bot-api');
